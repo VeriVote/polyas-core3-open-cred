d5a195f57da58e6aca3c537bd4312fb120ce5de9
Michael Kirsten <kirsten@kit.edu>
2020-02-11 15:41:52 +0100

    Iterator specification

--- boot/java/util/Collection.java
+++ boot/java/util/Collection.java
@@ -53,11 +53,12 @@ public interface Collection extends java.lang.Iterable
      @ ensures \result.index == 0;
      @ ensures \result.seq == seq;
      @ ensures \fresh(\result) && \fresh(\result.*);
+     @ ensures \typeof(\result) == \type(java.util.CollectionIterator);
      @ assignable \nothing;
      @ determines \result.seq \by seq;
      @ determines \result.index \by \nothing;
      @*/
-   public java.util.CollectionIterator iterator();
+   public java.util.Iterator iterator();
    
    public java.lang.Object[] toArray();
    public java.lang.Object[] toArray(java.lang.Object[] arg0);
