9ab0f04cbd99c81be8ef09715390ebf05422839b
Florian Lanzinger <uiekn@student.kit.edu>
2020-03-02 19:27:04 +0100

    Move Iterator specification to ListIterator

--- boot/org/bouncycastle/openpgp/PGPPublicKeyRingCollection.java
+++ boot/org/bouncycastle/openpgp/PGPPublicKeyRingCollection.java
@@ -7,9 +7,5 @@ public class PGPPublicKeyRingCollection extends java.lang.Object implements org.
      @*/
    public PGPPublicKeyRingCollection(java.io.InputStream param0, org.bouncycastle.openpgp.operator.KeyFingerPrintCalculator param1) throws java.io.IOException, org.bouncycastle.openpgp.PGPException;
 
-   /*@ public normal_behavior
-     @ ensures \result.index == 0;
-     @ assignable \nothing;
-     @*/
-   public java.util.CollectionIterator getKeyRings();
+   public java.util.Iterator getKeyRings();
 }
